d4fbc3e9cc9d0b817d7c20d9c4052dee61685daa
Rishi Raj <rishiraj824@gmail.com>
2016-12-11 21:35:24 +0530

    done

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -8,7 +8,12 @@ describe('App' , () => {
     component = renderComponent(App);
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
+  });
+  it('shows a comment list', () => {
+    expect(component.find('.comment-list')).to.exist;
   });
 });
+
+
